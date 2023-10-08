21b97384a0d58b7d47322a0527998dea5fea9a8d
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-10-08 20:09:29 +0100

    Update Index.js

--- backend/Index.js
+++ backend/Index.js
@@ -6,7 +6,11 @@ const app=express()
 const MembersRoutes=require("./routes/user")
 const MembersData=require("./routes/AdminRoutes")
 const connectDB=require("./db/database")
-app.use(cors())
+app.use(cors({
+origin:["https://bride-mu.vercel.app/"],
+credentials:true,
+methods: ['GET', 'POST'], // Specify the allowed HTTP methods
+}))
 app.use(express.json())
 // mongoose.connect(process.env.MONGODB_URL)
 connectDB()
